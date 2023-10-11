9157f6fa80706462a1381a67da21b6f947434299
Илья <ily-odinokov@yandex.ru>
2023-10-11 23:49:08 +0300

    select

--- src/QueryBuilder.Ms/Queries/MsSelectQueryBuilder.cs
+++ src/QueryBuilder.Ms/Queries/MsSelectQueryBuilder.cs
@@ -1,14 +1,25 @@
-﻿namespace QueryBuilder.Ms.Queries;
+﻿using QueryBuilder.Core.Queries;
+
+namespace QueryBuilder.Ms.Queries;
 
 public interface IMsSelectQueryBuilder<T>
 {
     IMsSelectQueryBuilder<T> Select(Action<IMsSelectBuilder<T>> inner);
 }
 
-public class MsSelectQueryBuilder<T> : IMsSelectQueryBuilder<T>
+public class MsSelectQueryBuilder<T> : QueryBuilderCore, IMsSelectQueryBuilder<T>
 {
+    protected MsSelectQueryBuilder(QueryBuilderSource source) : base(source) {}
+
     IMsSelectQueryBuilder<T> IMsSelectQueryBuilder<T>.Select(Action<IMsSelectBuilder<T>> inner)
     {
         throw new NotImplementedException();
+    }
+
+    public static MsSelectQueryBuilder<T> Make(QueryBuilderSource source, Action<MsSelectQueryBuilder<T>> inner)
+    {
+        var obj = new MsSelectQueryBuilder<T>(source);
+        inner?.Invoke(obj);
+        return obj;
     }
 }
